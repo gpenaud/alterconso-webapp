f87a37e45b26a62f7d49dd1ff21c95c0a1b22ea3
Administrator <admin@example.com>
2022-04-21 18:33:47 +0200

    Add the file crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -3,10 +3,8 @@
 touch /var/www/cagette/crontab.log
 
 # Added a cronjob in a new crontab
-echo "*  * * * * cd /var/www/cagette/www && /usr/bin/neko index.n cron/minute >> /var/www/cagette/crontab.log 2>&1" > /etc/crontab
-echo "0  * * * * cd /var/www/cagette/www && /usr/bin/neko index.n cron/hour >> /var/www/cagette/crontab.log 2>&1" >> /etc/crontab
-echo "15 0 * * * cd /var/www/cagette/www && /usr/bin/neko index.n cron/daily >> /var/www/cagette/crontab.log 2>&1" >> /etc/crontab
+echo "*  * * * * cd /var/www/cagette/www && /usr/bin/neko index.n cron/minute > /dev/null 2>&1" > /etc/crontab
+echo "0  * * * * cd /var/www/cagette/www && /usr/bin/neko index.n cron/hour > /dev/null 2>&1"  >> /etc/crontab
+echo "15 0 * * * cd /var/www/cagette/www && /usr/bin/neko index.n cron/daily > /dev/null 2>&1" >> /etc/crontab
 
 crontab /etc/crontab
-/usr/sbin/service cron start
-tail -f /var/www/cagette/crontab.log
